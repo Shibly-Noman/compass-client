08edb5f48af5573ca7bbefd0a1540fb392921b78
Shibly Noman <shiblynoman20@gmail.com>
2020-11-14 02:04:40 +0600

    initial commit

---
+++ pages/co/Teacher.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="grid grid-cols-12 gap-5">
+      <div class="col-span-1 bg-gray-400">
+        <div class="flex flex-col justify-center">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+      <div class="col-span-4 bg-gray-500 rounded">ProfileDiv</div>
+      <div class="col-span-1 bg-gray-600 rounded">Content</div>
+      <div class="col-span-1 bg-gray-700 rounded">Right Span</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Teacher",
+  data: () => ({}),
+  layout: "PrimaryProfile"
+};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>M</h1>
+</template>
